a4b8fd0255dc16d5b4129d12fca7e4c2d29da678
Stan van Zanten <stanzanten@hotmail.com>
2018-12-11 10:51:27 +0100

    Commit before Auth config

--- config/mongo.db.js
+++ config/mongo.db.js
@@ -11,4 +11,4 @@ var connection = mongoose.connection
         console.warn('Warning', error.toString());
     });
 
-module.exports = connection;
+module.exports = {connection, TOKEN_SECRET: process.env.TOKEN_SECRET || 'pvpnCCZfwOF85pBjbOebZiYIDhZ3w9LZrKwBZ7152K89mPCOHtbRlmr5Z91ci4L'}
