0b17be72743a736a0c0e98f508d03c59f169f908
Stan van Zanten <stanzanten@hotmail.com>
2018-12-12 19:54:46 +0100

    Server updated

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.use(logger('dev'));
 // CORS headers
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || "*");
+    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'https://games-frontend.herokuapp.com');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
